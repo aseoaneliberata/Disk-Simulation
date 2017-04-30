f7005b59ca8d953b3520327e3f2d35136b367921
Arturo Seoane Liberata <aseoaneliberata@unomaha.edu>
2017-04-29 23:46:24 -0400

    CLOOK implementation.

---
+++ ioreq.java
@@ -0,0 +1,31 @@
+
+public class ioreq {
+	/*------------*/
+	/* Input data */
+	/*------------*/
+	public int submit; /* time when request was submittted */
+	public int addr; /* logical disk address */
+	public int nsec; /* # of sectors to process */
+
+	/*--------------*/
+	/* Derived data */
+	/*--------------*/
+	public int acyl, atrk, asec; /* physical address of first sector */
+
+	/*--------------------*/
+	/* Simulation results */
+	/*--------------------*/
+	public int t_stop; /* time when request was completed */
+
+	/*-------*/
+	/* Flags */
+	/*-------*/
+	public boolean done; /* for all but FCFS, 0 if request is active */
+	public boolean now; /* for LOOK and C-LOOK */
+	
+	public ioreq (int submit, int addr, int nsec) {
+		this.submit = submit;
+		this.addr = addr;
+		this.nsec = nsec;
+	}
+}
